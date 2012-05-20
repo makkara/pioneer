840baebb057dacda976f59c73a527eeafc0037f4
unknown <jere@Makkara_mk2.(none)>
2012-05-20 12:53:24 +0300

    Database class throws exception when error occurs

--- src/Database.h
+++ src/Database.h
@@ -3,15 +3,25 @@
 #include <stdint.h>
 #include <string>
 #include <sqlite3/sqlite3.h>
+#include <stdexcept>
+
 
 class Database {
+public:
+	class Exception : public std::runtime_error
+	{
+	public:
+		Exception(const char*msg):std::runtime_error(msg){}
+	};
+
 protected:
 	sqlite3 *handle;
-	static bool Backup(Database&src,Database&dst);
+	static void Backup(Database&src,Database&dst);
+	void RaiseException(){throw Exception(sqlite3_errmsg(handle));}
 
 public:	
 	/* Simple wrapper around sqlite statement
-	 * Instantiate in stack, statement is automatically reset when object goes out of scope
+	 * statement is automatically reset when object is created
 	 */
 	class Statement
 	{
@@ -22,23 +32,23 @@ public:
 			Statement(sqlite3_stmt*stmt);
 			Statement(const Statement &s);
 			Statement& operator=(const Statement&s);
-			~Statement(){if(statement)sqlite3_reset(statement);};
+			~Statement(){};
+			void RaiseException(){throw Exception(sqlite3_errmsg(sqlite3_db_handle(statement)));};
 
 			//Bind data to statement wrappers
-			void Bind(int position, int32_t value){sqlite3_bind_int(statement,position,value);}
-			void Bind(int position, int64_t value){sqlite3_bind_int64(statement,position,value);}
-			void Bind(int position, uint32_t value){sqlite3_bind_int64(statement,position,value);}//unsigned 32bit is converted to 64bit for sorting to work correctly
-			void Bind(int position, double value){sqlite3_bind_double(statement,position,value);}
-			void BindNull(int position){sqlite3_bind_null(statement,position);}
+			void Bind(int position, int32_t value){if(sqlite3_bind_int(statement,position,value)!=SQLITE_OK)RaiseException();}
+			void Bind(int position, int64_t value){if(sqlite3_bind_int64(statement,position,value)!=SQLITE_OK)RaiseException();}
+			void Bind(int position, uint32_t value){if(sqlite3_bind_int64(statement,position,value)!=SQLITE_OK)RaiseException();}//unsigned 32bit is converted to 64bit for sorting to work correctly
+			void Bind(int position, double value){if(sqlite3_bind_double(statement,position,value)!=SQLITE_OK)RaiseException();}
+			void BindNull(int position){if(sqlite3_bind_null(statement,position)!=SQLITE_OK)RaiseException();}
 
 			//get row data
 			int64_t Get_int64(int column){return sqlite3_column_int64(statement, column);}
 			int32_t Get_int32(int column){return sqlite3_column_int(statement, column);}
 			double Get_double(int column){return sqlite3_column_double(statement, column);}
 
-			//Operations on statement			
-			bool NextRow(){return sqlite3_step(statement)==SQLITE_ROW;}//return true if row is received, TODO: errors should be handled with exceptions
-			bool Run(){return sqlite3_step(statement)==SQLITE_DONE;}//return true if statement completes succesfully
+			//Step prepared statement, return true if row is returned
+			bool Step(){int rv=sqlite3_step(statement);if(rv!=SQLITE_DONE && rv!=SQLITE_ROW)RaiseException();return (rv==SQLITE_ROW);};
 
 			//convert to sqlite3 statement handle
 			operator sqlite3_stmt*(){return statement;}
@@ -49,24 +59,24 @@ public:
 	 */
 	Statement  PrepareStatement(const char * statement);
 
-	bool Create();//create in memory database, if database already exists close previous
+	void Create();//create in memory database, if database already exists close previous
 	/*
 	 * Open database on disk
 	 */
-	bool Open(std::string filename);
+	void Open(std::string filename);
 	/**
 	 * Creates new in memory database and backups file to memory
 	 */
-	bool Load(std::string filename);
+	void Load(std::string filename);
 	/**
 	 * Deletes existing file and backups in-memory database to it
 	 */
-	bool Save(std::string filename);
+	void Save(std::string filename);
 	/**
-	 * Execute SQL statement(s), returns true if succesful
+	 * Execute SQL statement(s)
 	 * Slow operation, statement(s) is compiled everytime this is called
 	 */
-	bool Execute(const char * statement);
+	void Execute(const char * statement);
 
 	Database();
 	virtual ~Database();
