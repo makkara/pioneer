d18d0de78ff0b3c237230e48ee2cec503d399a51
unknown <jere@Makkara_mk2.(none)>
2012-05-20 11:21:55 +0300

    Add statement wrapper to database

--- src/Database.h
+++ src/Database.h
@@ -1,5 +1,6 @@
 #ifndef _DB_H
 #define _DB_H
+#include <stdint.h>
 #include <string>
 #include <sqlite3/sqlite3.h>
 
@@ -8,10 +9,46 @@ protected:
 	sqlite3 *handle;
 	static bool Backup(Database&src,Database&dst);
 
-	/* Compile Statement and return handle
-	 */
-	sqlite3_stmt * PrepareStatement(const char * statement);
 public:	
+	/* Simple wrapper around sqlite statement
+	 * Instantiate in stack, statement is automatically reset when object goes out of scope
+	 */
+	class Statement
+	{
+		public:
+			sqlite3_stmt * statement;
+
+			Statement();
+			Statement(sqlite3_stmt*stmt);
+			Statement(const Statement &s);
+			Statement& operator=(const Statement&s);
+			~Statement(){if(statement)sqlite3_reset(statement);};
+
+			//Bind data to statement wrappers
+			void Bind(int position, int32_t value){sqlite3_bind_int(statement,position,value);}
+			void Bind(int position, int64_t value){sqlite3_bind_int64(statement,position,value);}
+			void Bind(int position, uint32_t value){sqlite3_bind_int64(statement,position,value);}//unsigned 32bit is converted to 64bit for sorting to work correctly
+			void Bind(int position, double value){sqlite3_bind_double(statement,position,value);}
+			void BindNull(int position){sqlite3_bind_null(statement,position);}
+
+			//get row data
+			int64_t Get_int64(int column){return sqlite3_column_int64(statement, column);}
+			int32_t Get_int32(int column){return sqlite3_column_int(statement, column);}
+			double Get_double(int column){return sqlite3_column_double(statement, column);}
+
+			//Operations on statement			
+			bool NextRow(){return sqlite3_step(statement)==SQLITE_ROW;}//return true if row is received, TODO: errors should be handled with exceptions
+			bool Run(){return sqlite3_step(statement)==SQLITE_DONE;}//return true if statement completes succesfully
+
+			//convert to sqlite3 statement handle
+			operator sqlite3_stmt*(){return statement;}
+	};
+
+	/* 
+	 * Compile Statement and return handle
+	 */
+	Statement  PrepareStatement(const char * statement);
+
 	bool Create();//create in memory database, if database already exists close previous
 	/*
 	 * Open database on disk
